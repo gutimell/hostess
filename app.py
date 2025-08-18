034584dcf646d3fab0f974424ef78c41f1e41dd7
ArwenX <96956383+ArwenX@users.noreply.github.com>
2025-08-18 19:33:51 -0400

    Actualizar app.py

--- app.py
+++ app.py
@@ -15,13 +15,13 @@ app.secret_key = 'tu_clave_secreta_aqui_es_muy_importante'
 
 HUESPEDES_VALIDOS = [
     {'depto': '1005', 'nombre': 'admin'},
-{'depto': '1005', 'nombre': 'Mauricio'},
 {'depto': '1005', 'nombre': 'Silvana'},
 {'depto': '1005', 'nombre': 'Luana'},
+
     {'depto': '1006', 'nombre': 'admin'},
 {'depto': '1006', 'nombre': 'Kenneth'},
-{'depto': '1006', 'nombre': 'Marcela'},
-{'depto': '1006', 'nombre': 'Javiera'},
+{'depto': '1006', 'nombre': 'Mauricio'},
+{'depto': '1006', 'nombre': 'Felipe'},
 ]
 
 mock_data = {
