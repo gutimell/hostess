2098c22987917c0be48930786644b0e0d9a6f447
ArwenX <96956383+ArwenX@users.noreply.github.com>
2025-09-13 11:19:00 -0300

    Actualizar app.py

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ app.secret_key = 'tu_clave_secreta_aqui_es_muy_importante'
 
 HUESPEDES_VALIDOS = [
     {'depto': '1005', 'nombre': 'admin'},
-{'depto': '1005', 'nombre': 'Jason'},
-{'depto': '1005', 'nombre': 'Esteban'},
-{'depto': '1005', 'nombre': 'Mauricio'},
+{'depto': '1005', 'nombre': 'X'},
+{'depto': '1005', 'nombre': 'X'},
+{'depto': '1005', 'nombre': 'Camila'},
 
     {'depto': '1006', 'nombre': 'admin'},
 {'depto': '1006', 'nombre': 'Jason'},
