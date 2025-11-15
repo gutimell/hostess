6917f27763a683980bcff1ac6e7c92474d6746e4
ArwenX <96956383+ArwenX@users.noreply.github.com>
2025-11-15 09:56:24 -0300

    Actualizar app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ HUESPEDES_VALIDOS = [
 
     {'depto': '1006', 'nombre': 'admin'},
 {'depto': '1006', 'nombre': 'Jeremy'},
-{'depto': '1006', 'nombre': 'Jessica'},
+{'depto': '1006', 'nombre': 'Miguel'},
 {'depto': '1006', 'nombre': 'Cristian'},
 ]
 
