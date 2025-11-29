771f0cf61c680e4420958e458f71b86dae1d40e1
ArwenX <96956383+ArwenX@users.noreply.github.com>
2025-11-28 22:48:25 -0300

    Actualizar app.py

--- app.py
+++ app.py
@@ -17,13 +17,13 @@ HUESPEDES_VALIDOS = [
     {'depto': '1005', 'nombre': 'admin'},
 {'depto': '1005', 'nombre': 'AX'},
 {'depto': '1005', 'nombre': 'Jeremy'},
-{'depto': '1005', 'nombre': 'Andrea'},
+{'depto': '1005', 'nombre': 'Andres'},
 {'depto': '1005', 'nombre': 'Cihara'},
 
     {'depto': '1006', 'nombre': 'admin'},
 {'depto': '1006', 'nombre': 'Jeremy'},
 {'depto': '1006', 'nombre': 'Natalia'},
-{'depto': '1006', 'nombre': 'Cristian'},
+{'depto': '1006', 'nombre': 'Giselle'},
 ]
 
 mock_data = {
